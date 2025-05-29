154b4e5c34515a7314b4581225d6a54edc9bb762
jpboy2 <jimmueltolentino08082@gmail.com>
2025-05-29 18:00:26 +0800

    init

---
+++ app/Livewire/IssuesList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class IssuesList extends Component
+{
+    public function render()
+    {
+        return view('livewire.issues-list');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Livewire\Counter;
+ 
+Route::get('/counter', Counter::class);
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
